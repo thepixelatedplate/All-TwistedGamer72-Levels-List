b4886cbc8a580730e4c214f14c50dd8447a3425d
thepixelatedplate <sholtonbolton@gmail.com>
2026-04-21 20:35:55 -0300

    Fixed data paths and records

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = '/data';
+const dir = './data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
@@ -18,9 +18,7 @@ export async function fetchList() {
                         {
                             ...level,
                             path,
-                            records: level.records.sort(
-                                (a, b) => b.percent - a.percent,
-                            ),
+                           records: (level.records || []).sort((a, b) => b.percent - a.percent),
                         },
                         null,
                     ];
